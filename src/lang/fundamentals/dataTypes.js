b3336e6fc567a798aa1f5ca06536db22e650aba5
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-01-27 18:50:40 +0200

    JS fundamentals - data types

---
+++ src/lang/fundamentals/dataTypes.js
@@ -0,0 +1,9 @@
+console.log(`typeof undefined - ${typeof undefined}`);
+console.log(`typeof 0 - ${typeof 0}`);
+console.log(`typeof 10n - ${typeof 10n}`);
+console.log(`typeof true - ${typeof true}`);
+console.log(`typeof "foo" - ${typeof 'foo'}`);
+console.log(`typeof Symbol("id") - ${typeof Symbol('id')}`);
+console.log(`typeof Math - ${typeof Math}`);
+console.log(`typeof null - ${typeof null}`);
+console.log(`typeof alert - ${typeof alert}`);
