e75b04a62cec50d179562544c9b2881ecb463ae3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-01-27 19:07:23 +0200

    Interaction with a user

---
+++ src/lang/fundamentals/interaction.js
@@ -0,0 +1,15 @@
+alert('Something importand is writing here!');
+
+// may be that what 
+// users entered or
+// null if they canceled 
+// or default value if they pressed ok
+let favouriteDrink = prompt('What is your favouite drink?', 'whater');
+console.log(favouriteDrink);
+
+// the result is true if Ok pressed otherwise false
+let isLikeApples = confirm('Do you like apples?');
+console.log(isLikeApples);
+
+let userName = prompt('What is your name?', 'Guest');
+alert(userName);
