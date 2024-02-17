e28521d1bcabe914b09e8386506d905f08193508
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-02-17 21:56:09 +0200

    Switch statement

---
+++ src/lang/fundamentals/switch.js
@@ -0,0 +1,44 @@
+let a = 2 + 3;
+
+switch (a) {
+	case 3:
+		console.log('too small!');
+		break;
+	case 5:
+		console.log('exactly!');
+		break;
+	case 7:
+		console.log('too big!');
+	default:
+		console.log('unknown value');
+}
+
+
+// task 1
+let browser = '';
+if (browser === 'Edge') {
+	console.log("You've got the Edge!");
+} else if (	browser === 'Chrome' ||
+					 	browser === 'Firefox' ||
+					 	browser === 'Safari' ||
+					 	browser === 'Opera' ) {
+	console.log('Okay we support these browsers too');
+} else {
+	console.log('We hope that this page looks ok!');
+}
+
+
+// task 2
+let b = +prompt('b?', '');
+switch (b) {
+	case 0:
+		console.log(0);
+		break;
+	case 1:
+		console.log(1);
+		break;
+	case 2:
+	case 3:
+		console.log(2, 3);
+		break;
+}
