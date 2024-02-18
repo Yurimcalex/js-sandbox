3d7c50b4c7b97f4b3834f6a3438e8e13a9a755d3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-02-18 10:30:28 +0200

    Arrow functions, the basics

---
+++ src/lang/fundamentals/arrowFuncBasis.js
@@ -0,0 +1,30 @@
+let age = 18;
+let welcome = (age > 18) ?
+	() => console.log('Hello!') :
+	() => console.log('Hi!');
+
+welcome();
+
+
+let sum = (a, b) => {
+	let result = a + b;
+	return result;
+}
+
+console.log(sum(3, 4));
+
+
+// task
+function ask(question, yes, no) {
+	if (confirm(question)) {
+		yes();
+	} else {
+		no();
+	}
+}
+
+ask(
+	'Do you have a cat?',
+	() => console.log('Miaw!'),
+	() => console.log('But why?')
+);
