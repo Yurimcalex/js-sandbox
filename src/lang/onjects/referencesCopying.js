9c58e66b5e359728fea3c0f5afa7876528dc0fe9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-02-18 19:56:48 +0200

    Object references and copying

---
+++ src/lang/onjects/referencesCopying.js
@@ -0,0 +1,49 @@
+let animal = 'cat';
+let critter = animal;
+//console.log(critter);
+
+let user = {
+	name: 'Piter'
+};
+
+let person = user;
+//console.log(person.name);
+
+person.age = 20;
+//console.log(user.age);
+
+let employee = {
+	name: 'Piter'
+};
+//console.log(person === user, employee === user);
+
+let clone = {};
+for (let prop in user) clone[prop] = user[prop];
+//console.log(clone);
+
+
+let player = {
+	name: 'Castilia',
+	age: 30,
+};
+
+let feature1 = { canSwim: true };
+let feature2 = { canPlayTennis: true };
+
+Object.assign(player, feature1, feature2);
+//console.log(player);
+
+let playerClone = Object.assign({}, player);
+console.log(playerClone);
+
+
+let door = {
+	type: 'inner',
+	sizes: {
+		height: 300,
+		width: 100
+	}
+};
+
+//let doorDeepClone = structuredClone(door);
+//console.log(doorDeepClone);
