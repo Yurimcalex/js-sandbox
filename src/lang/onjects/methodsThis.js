65725480eea97d8b62805009da5e012603c1710d
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-02-19 17:46:49 +0200

    Objects methids, this

---
+++ src/lang/onjects/methodsThis.js
@@ -0,0 +1,87 @@
+let user = {
+	name: 'Dilan',
+	age: 35
+};
+
+user.sayHi = function () {
+	console.log('Hello!')
+};
+
+//user.sayHi();
+
+
+let box = {
+	size: 16,
+	greeting() {
+		console.log('Hello!, I am a box');
+	},
+	showSize() {
+		console.log(this.size);
+	}
+};
+
+//box.greeting();
+//box.showSize();
+
+let admin = {
+	name: 'Joshua'
+};
+
+function showName() {
+	console.log(this.name);
+}
+
+user.showName = showName;
+admin.showName = showName;
+
+//user.showName();
+//admin.showName();
+
+function sayPhraze(phraze) {
+	let f = () => `${this.name}: ${phraze}`;
+	console.log(f());
+}
+
+user.sayPhraze = sayPhraze;
+user.sayPhraze('Hello');
+
+
+// task 2
+let calculator = {
+	a: null,
+	b: null,
+	read() {
+		this.a = +prompt('Enter first number', 0);
+		this.b = +prompt('Enter second number', 0);
+	},
+	sum() {
+		console.log(this.a + this.b);
+	},
+	mul() {
+		console.log(this.a * this.b);
+	}
+};
+
+//calculator.read();
+//calculator.sum();
+//calculator.mul();
+
+
+// task 3
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function() { // shows the current step
+    console.log( this.step );
+    return this;
+  }
+};
+
+ladder.up().up().down().showStep().down().showStep();
