8b5f862cd87c1066bfdf4da6c4281434b00667df
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-28 12:22:42 +0200

    Task 8 - filter through function

--- src/lang/funcs/scopeClosure.js
+++ src/lang/funcs/scopeClosure.js
@@ -135,4 +135,23 @@ function func() {
 	let x = 2;
 }
 
-func();
+//func();
+
+
+// Task 8 - filter through function
+function inBetween(a, b) {
+	return function (n) {
+		return a <= n && n <= b;
+	}
+}
+
+function inArray(arr) {
+	return function (n) {
+		return arr.includes(n);
+	}
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(arr.filter(inBetween(3, 6)));
+console.log(arr.filter(inArray([1, 2, 10])));
