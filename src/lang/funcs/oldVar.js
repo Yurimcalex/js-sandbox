2e34ca1538742351dbea1010e3e5c65e9e9e5bd7
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-28 19:20:58 +0200

    The old var

---
+++ src/lang/funcs/oldVar.js
@@ -0,0 +1,36 @@
+// var has no block scope
+if (true) {
+	var x = 1;
+}
+console.log(x);
+
+for (var i = 0; i < 10; i += 1) {}
+console.log(i);
+
+function f() {
+	var y = 2;
+}
+//console.log(y);
+
+
+// var tolerates redeclaretions
+var leaf = 'big';
+var leaf = 'small';
+console.log(leaf);
+
+function g() {
+	console.log(y);
+	if (false) {
+		var y;
+	}
+	y = 10;
+}
+
+g();
+
+
+// IIFE
+(function () {
+	var someValue;
+})();
+//console.log(someValue);
