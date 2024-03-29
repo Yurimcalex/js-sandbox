ebc46d8b21ca4158e8d6b293adb3de49739ec36b
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-30 00:31:19 +0200

    Task 4 - throttle decorator

--- src/lang/funcs/decoratorsForwarding.js
+++ src/lang/funcs/decoratorsForwarding.js
@@ -175,9 +175,47 @@ let sayHey = function (name) {
 	console.log('Hey! ' + name);
 };
 
-let shd = debounce(sayHey, 1000);
+let shd = debounce1(sayHey, 1000);
 
-shd('John');
-setTimeout(() => shd('Piter'), 500);
-setTimeout(() => shd('Sam'), 1700);
-setTimeout(() => shd('Gorge'), 800);
+// shd('John');
+// setTimeout(() => shd('Piter'), 500);
+// setTimeout(() => shd('Sam'), 1700);
+// setTimeout(() => shd('Gorge'), 800);
+
+
+// Throttle decorator
+function f(a) {
+	console.log(a);
+}
+
+function throttle(f, ms) {
+	let isThrottled = false,
+	    savedArgs,
+	    savedThis;
+
+	function wrapper() {
+		if (isThrottled) {
+			savedArgs = arguments;
+			savedThis = this;
+			return;
+		}
+		isThrottled = true;
+		f.apply(this, arguments);
+
+		setTimeout(function () {
+			isThrottled = false;
+			if (savedArgs) {
+				wrapper.apply(savedThis, savedArgs);
+				savedArgs = savedThis = null;
+			}
+		}, ms);
+	}
+
+	return wrapper;
+}
+
+let f1000 = throttle(f, 1000);
+
+f1000(1); // shows 1
+f1000(2); // (throttling, 1000ms not out yet)
+f1000(3); // (throttling, 1000ms not out yet)
