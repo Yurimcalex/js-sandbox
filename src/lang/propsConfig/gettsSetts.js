9f3cea89e439cee75f92afbd7ffc9eb2a0f52f72
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-30 18:14:29 +0200

    Property getters and setters

---
+++ src/lang/propsConfig/gettsSetts.js
@@ -0,0 +1,61 @@
+// Getters and setters
+let user = {
+	fname: 'Buba',
+	lname:'Smith',
+	get fullname() {
+		console.log(`I'm ${this.fname} ${this.lname}`);
+	},
+
+	set fullname(value) {
+		[this.fname, this.lname] = value.split(' ');
+	}
+};
+
+user.fullname = 'Hiley Smith';
+console.log(user.fullname);
+
+// Accessor descriptors
+user.items = [];
+Object.defineProperty(user, 'pocket', {
+	get() {
+		return this.items.join(', ') || 'empty'
+	},
+	set(value) {
+		this.items.push(value);
+	},
+	enumerable: true
+});
+
+user.pocket = 'banana';
+user.pocket = 'pen';
+console.log(user.pocket);
+
+// Smarter getters/setters
+let player = {
+	get name() {
+		return this._name;
+	},
+	set name(value) {
+		if (value.length < 3) {
+			console.log('Its too short!');
+			return;
+		}
+		this._name = value;
+	}
+};
+
+player.name = 'Ou';
+
+// Using for compatibility
+function User(name, birthday) {
+	this.name = name;
+	this.birthday = birthday;
+	Object.defineProperty(this, 'age', {
+		get () {
+			return new Date().getFullYear() - this.birthday.getFullYear();
+		}
+	});
+}
+
+let john = new User('John', new Date(1980));
+console.log(john.age);
