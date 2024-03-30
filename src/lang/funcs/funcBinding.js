cb1362dc0727fde1fc163ff049c6e3a56c47cf9e
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-30 16:16:52 +0200

    Task 4 - fix a function that loses this

--- src/lang/funcs/funcBinding.js
+++ src/lang/funcs/funcBinding.js
@@ -85,3 +85,31 @@ man.sayNow('Greeting!');
 	let bound = sayHi.bind({ name: 'Lola' });
 	console.log(bound.x);
 }
+
+
+// Task 4 - fix a function that loses this
+{
+	function askPassword(ok, fail) {
+	  let password = prompt("Password?", '');
+	  if (password == "rockstar") ok();
+	  else fail();
+	}
+
+	let user = {
+	  name: 'John',
+
+	  loginOk() {
+	    alert(`${this.name} logged in`);
+	  },
+
+	  loginFail() {
+	    alert(`${this.name} failed to log in`);
+	  },
+
+	};
+
+	askPassword(
+		user.loginOk.bind(user),
+		user.loginFail.bind(user)
+	);
+}
