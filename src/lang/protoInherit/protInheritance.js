d6628581dbc39d3357a52b73fc03b464cfd12cd7
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-30 21:18:18 +0200

    Task 2 - searching algorithm

--- src/lang/protoInherit/protInheritance.js
+++ src/lang/protoInherit/protInheritance.js
@@ -115,3 +115,27 @@ admin.fullname;
 	delete animal.jumps;
 	console.log(rabbit.jumps);
 }
+
+
+// Task 2 - searching algorithm
+let head = {
+  glasses: 1
+};
+
+let table = {
+  pen: 3,
+  __proto__: head
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2,
+  __proto__: table
+};
+
+let pockets = {
+  money: 2000,
+  __proto__: bed
+};
+
+console.log(pockets.glasses, head.glasses);
