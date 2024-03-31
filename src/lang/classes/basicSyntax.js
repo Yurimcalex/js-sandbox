b271de57ad172f7ba3a5ec62c6d967f3af0098d1
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-03-31 19:47:36 +0300

    Task 1 - rewrite to class

--- src/lang/classes/basicSyntax.js
+++ src/lang/classes/basicSyntax.js
@@ -94,3 +94,59 @@ class Button {
 
 let button = new Button("hello");
 setTimeout(button.click, 1000);
+
+
+// Task 1 - rewrite to class
+// function Clock({ template }) {
+//   let timer;
+//   function render() {
+//     let date = new Date();
+//     let hours = date.getHours();
+//     if (hours < 10) hours = '0' + hours;
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = '0' + mins;
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = '0' + secs;
+//     let output = template
+//       .replace('h', hours)
+//       .replace('m', mins)
+//       .replace('s', secs);
+//     console.log(output);
+//   }
+//   this.stop = function() {
+//     clearInterval(timer);
+//   };
+//   this.start = function() {
+//     render();
+//     timer = setInterval(render, 1000);
+//   };
+// }
+
+class Clock {
+	timer;
+	constructor({ template }) {
+		this.template = template;
+	}
+	stop() { clearInterval(this.timer) }
+	start() {
+		this._render();
+		this.timer = setInterval(() => this._render(), 1000);
+	}
+	_render() {
+		let date = new Date();
+		let hours = date.getHours();
+		if (hours < 10) hours = '0' + hours;
+		let mins = date.getMinutes();
+		if (mins < 10) mins = '0' + mins;
+		let secs = date.getSeconds();
+		if (secs < 10) secs = '0' + secs;
+		let output = this.template
+		  .replace('h', hours)
+		  .replace('m', mins)
+		  .replace('s', secs);
+		console.log(output);
+	}
+}
+
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
