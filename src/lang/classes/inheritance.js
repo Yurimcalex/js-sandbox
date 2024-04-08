6b302194b39440c21a133e6635d126ee9ccf30fa
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-08 16:50:25 +0300

    Task 1 - Error creating an instance

--- src/lang/classes/inheritance.js
+++ src/lang/classes/inheritance.js
@@ -79,3 +79,23 @@ class ChildClass extends ParentClass {
 
 new ParentClass();
 new ChildClass();
+
+
+// Task 1 - Error creating an instance
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+  	super(name);
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+console.log(rabbit.name);
