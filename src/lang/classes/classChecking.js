a4482057e1d9b52f342af4b40b3a4de105f8ead8
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-08 19:29:21 +0300

    Class checking: instanceof

---
+++ src/lang/classes/classChecking.js
@@ -0,0 +1,37 @@
+class Ball {}
+let ball = new Ball();
+console.log(ball instanceof Ball);
+
+console.log([] instanceof Array);
+console.log([] instanceof Object);
+
+
+class Fly {
+	static [Symbol.hasInstance](obj) {
+		if (obj.canFly) return true;
+	}
+}
+
+let fly = { canFly: true };
+console.log(fly instanceof Fly);
+
+
+class Dog {}
+class HappyDog extends Dog {}
+let dog = new HappyDog();
+console.log(dog instanceof Dog);
+
+function Rabbit() {}
+let rabbit = new Rabbit();
+Rabbit.prototype = {};
+console.log(rabbit instanceof Rabbit);
+
+let objToStr = Object.prototype.toString;
+console.log(objToStr.call([]));
+
+
+let pen = {
+	[Symbol.toStringTag]: 'Pen'
+};
+
+console.log({}.toString.call(pen));
