7630945dbee77c3fe1782eb345806d196a968936
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-13 18:54:31 +0300

    Example: loadScript

--- src/lang/promiseAsync/chaining.js
+++ src/lang/promiseAsync/chaining.js
@@ -29,3 +29,40 @@ new Promise(function (resolve, reject) {
 }).then(result => {
 	console.log(result);
 });
+
+
+// Example: loadScript
+function loadScript(src, callback) {
+	return new Promise(function (resolve, reject) {
+		let script = document.createElement('script');
+		script.src = src;
+
+		script.onload = () => resolve(script);
+		script.onerror = () => reject(new Error(`Script load error for ${src}`));
+
+		document.head.append(script);
+	});
+}
+
+loadScript('pathToScript')
+	.then(script => {
+		return loadScript('pathToAnotherScript');
+	})
+	.then(script => {
+		return loadScript('oneMoreScript');
+	})
+	.then(script => {
+		// use script's funcs
+	})
+	.catch(err => console.log(err.message));
+
+
+loadScript('path').then(script => {
+	loadScript('path1').then(script1 => {
+		loadScript('path2').then(script2 => {
+			// this way we have access to the all scripts
+			// script, script1 and script2
+		})
+	})
+})
+	.catch(err => console.log(err.message));
