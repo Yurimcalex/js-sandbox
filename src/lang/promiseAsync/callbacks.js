55eac3f6809e67518550fc2ed99cd63062245042
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-13 14:17:11 +0300

    Callbacks - introduction

---
+++ src/lang/promiseAsync/callbacks.js
@@ -0,0 +1,12 @@
+// Introduction: callbacks
+function loadScript(src, callback) {
+	let script = document.createElement('script');
+	script.src = src;
+	script.onload = () => callback(script);
+	document.head.append(script);
+}
+
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+  console.log(`Cool, the script ${script.src} is loaded`);
+  console.log( _ ); // _ is a function declared in the loaded script
+});
