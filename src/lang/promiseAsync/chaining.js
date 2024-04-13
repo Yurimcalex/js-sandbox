8c6b988a020839130cb777a08e5e76b4d6fbfd32
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-13 18:44:23 +0300

    Returning promises

--- src/lang/promiseAsync/chaining.js
+++ src/lang/promiseAsync/chaining.js
@@ -11,3 +11,21 @@ new Promise(function (resolve, reject) {
 	console.log(result);
 	return result * 8;
 });
+
+
+// Returning promises
+new Promise(function (resolve, reject) {
+	setTimeout(() => resolve(10), 1500);
+}).then(result => {
+	console.log(result);
+	return new Promise(function (resolve, reject) {
+		setTimeout(() => resolve(result * 10), 1000);
+	});
+}).then(result => {
+	console.log(result);
+	return new Promise(function (resolve, reject) {
+		setTimeout(() => resolve(result * 10), 500);
+	});
+}).then(result => {
+	console.log(result);
+});
