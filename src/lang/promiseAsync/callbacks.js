bd51084402b451e79e278c510e29f4a6de0157a4
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-13 14:23:26 +0300

    Callback in callback

--- src/lang/promiseAsync/callbacks.js
+++ src/lang/promiseAsync/callbacks.js
@@ -10,3 +10,20 @@ loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', s
   console.log(`Cool, the script ${script.src} is loaded`);
   console.log( _ ); // _ is a function declared in the loaded script
 });
+
+
+// Callback in callback
+function coolAsyncAction(action, callback) {
+	setTimeout(() => {
+		console.log(action);
+		callback();
+	}, 1000);
+}
+
+coolAsyncAction('Hello', () => {
+	coolAsyncAction('World', () => {
+		coolAsyncAction('!', () => {
+			console.log('Whhuuu done!');
+		});
+	});
+});
