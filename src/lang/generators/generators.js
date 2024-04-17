23ec07e36fd10c556c5dc06ee90ad9a8165394ba
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-17 19:16:53 +0300

    Generators are iterable

--- src/lang/generators/generators.js
+++ src/lang/generators/generators.js
@@ -14,3 +14,23 @@ let two = generator.next();
 let three = generator.next();
 
 console.log(JSON.stringify(three));
+
+
+// Generators are iterable
+let gen1 = generateSequence();
+for (let value of gen1) {
+	console.log(value);
+}
+
+function* genSeq1() {
+	yield 10;
+	yield 20;
+	yield 30;
+}
+
+for (let v of genSeq1()) {
+	console.log(v);
+}
+
+let numbs = [0, ...genSeq1()];
+console.log(numbs);
