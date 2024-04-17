7b246ec673465ef0c6715e49ec178905e6a91477
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-17 14:03:06 +0300

    Task 1 - Rewrite using async/await

--- src/lang/promiseAsync/asyncAwait.js
+++ src/lang/promiseAsync/asyncAwait.js
@@ -82,3 +82,16 @@ async function f4() {
 }
 
 f4().catch(err => console.log(err.message));
+
+
+// Task 1 - Rewrite using async/await
+async function loadJson(url) {
+	let response = await fetch(url);
+	if (response.status === 200) {
+		return await response.json();
+	}
+	throw new Error(response.status);
+}
+
+loadJson('https://javascript.info/no-such-user.json')
+  .catch(err => console.log(err.toString()));
