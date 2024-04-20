de6597566d29eb534640a1cf3ca318e68c2118a1
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-20 16:01:14 +0300

    Real-life example: paginated data

--- src/lang/generators/asyncIterGen.js
+++ src/lang/generators/asyncIterGen.js
@@ -103,3 +103,31 @@ let rangeN = {
     console.log(value, ' <==');
   }
 })();
+
+
+// Real-life example: paginated data
+async function* fetchCommits(repo) {
+	let url = `https://api.github.com/repos/${repo}/commits`;
+	while (url) {
+		const response = await fetch(url, {
+			headers: { 'User-Agent': 'Our script' },
+		});
+		const body = await response.json();
+		let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
+		nextPage = nextPage?.[1];
+		url = nextPage;
+		for (let commit of body) {
+			yield commit;
+		}
+	}
+}
+
+(async () => {
+	let count = 0;
+	for await (const commit of fetchCommits('javascript-tutorial/en.javascript.info')) {
+		console.log(commit.author?.login);
+		if (++count === 100) {
+			break;
+		}
+	}
+})();
