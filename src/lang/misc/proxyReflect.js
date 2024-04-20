d603407294b62c131c37c98ab5c624ea5cbdab1a
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-20 19:00:25 +0300

    Default value with 'get' trap

--- src/lang/misc/proxyReflect.js
+++ src/lang/misc/proxyReflect.js
@@ -6,3 +6,32 @@ proxy.test = 10;
 console.log(target.test, proxy.test);
 for (let key in proxy) console.log(key);
 
+
+// Default value with 'get' trap
+let numbers = [0, 1, 2];
+numbers = new Proxy(numbers, {
+	get(target, prop) {
+		if (prop in target) {
+			return target[prop];
+		} else {
+			return 0;
+		}
+	}
+});
+
+console.log(numbers[1], numbers[900]);
+
+
+let dictionary = {
+	'Hello': 'Hola',
+	'Bye': 'Adios'
+};
+
+dictionary = new Proxy(dictionary, {
+	get(target, prop) {
+		if (prop in target) return target[prop];
+		else return prop;
+	}
+});
+
+console.log(dictionary['Hello'], dictionary['Welcome']);
