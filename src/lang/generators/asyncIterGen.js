dd1819caf178b40e602ac85ecf566b20c915b7c9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-20 15:45:15 +0300

    Async iterable range

--- src/lang/generators/asyncIterGen.js
+++ src/lang/generators/asyncIterGen.js
@@ -84,3 +84,22 @@ async function* genSequence(start, end) {
 		console.log(n, ' <--');
 	}
 })();
+
+
+// Async iterable range
+let rangeN = {
+  from: 1,
+  to: 5,
+  async *[Symbol.asyncIterator]() {
+    for(let value = this.from; value <= this.to; value++) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      yield value;
+    }
+  }
+};
+
+(async () => {
+  for await (let value of rangeN) {
+    console.log(value, ' <==');
+  }
+})();
