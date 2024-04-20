a03bb31160649c7a3e6ebc33f6dbd92dd2084ebb
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-20 22:48:41 +0300

    In range with 'has' trap

--- src/lang/misc/proxyReflect.js
+++ src/lang/misc/proxyReflect.js
@@ -146,3 +146,19 @@ try {
 }
 
 for (let key in fruit) console.log(key);
+
+
+// In range with has trap
+let range = {
+	start: 1,
+	end: 10
+};
+
+range = new Proxy(range, {
+	has(target, prop) {
+		return prop >= target.start && prop <= target.end;
+	}
+});
+
+console.log(3 in range);
+console.log(30 in range);
