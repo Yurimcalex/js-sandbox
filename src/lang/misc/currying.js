cc86950f349e12ec5d2cc13ca9ca36ed7c718a94
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-21 20:39:55 +0300

    Currying what for

--- src/lang/misc/currying.js
+++ src/lang/misc/currying.js
@@ -13,3 +13,31 @@ function sum(a, b) {
 
 let curriedSum = curry(sum);
 console.log(curriedSum(1)(2));
+
+
+// Currying? What for?
+function log(date, importance, message) {
+  console.log(`[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`);
+}
+
+function curry3(f) {
+	return function (a) {
+		return function (b) {
+			return function (c) {
+				return f(a, b, c);
+			}
+		}
+	}
+}
+
+let curriedLog = curry3(log);
+log(new Date(), 'OPAPA', 'Cooooooooooooool!!!');
+curriedLog(new Date())('Message')('Hi!');
+
+let logNow = curriedLog(new Date());
+logNow('TEXT')('Ouuum');
+logNow('TEXT')('UUUUrrraaaaaaa');
+
+let textNow = logNow('Text');
+textNow('Greeting!');
+textNow('Hi everyone!');
