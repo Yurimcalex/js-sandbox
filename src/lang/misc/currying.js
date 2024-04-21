01abf722e59344fac085011e0e5b4f678c52cff3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-21 20:54:20 +0300

    Advanced curry implementation

--- src/lang/misc/currying.js
+++ src/lang/misc/currying.js
@@ -41,3 +41,28 @@ logNow('TEXT')('UUUUrrraaaaaaa');
 let textNow = logNow('Text');
 textNow('Greeting!');
 textNow('Hi everyone!');
+
+
+// Advanced curry implementation
+{
+	function curry(func) {
+		return function curried(...args) {
+			if (args.length >= func.length) {
+				return func.apply(this, args);
+			} else {
+				return function (...args2) {
+					return curried.apply(this, args.concat(args2));
+				}
+			}
+		}
+	}
+
+	function sum(a, b, c) {
+		return a + b + c;
+	}
+
+	let curriedSum = curry(sum);
+	console.log(curriedSum(1, 2, 3));
+	console.log(curriedSum(1)(2, 3));
+	console.log(curriedSum(1)(2)(3));
+}
