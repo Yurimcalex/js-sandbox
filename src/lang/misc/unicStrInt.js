b2ceee3d60747a26d3cee4cea620ce7c18e76250
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-23 17:40:21 +0300

    Unicode, String Internals

---
+++ src/lang/misc/unicStrInt.js
@@ -0,0 +1,9 @@
+// Unicode, String Internals
+console.log('\x7A');
+console.log('\xA9');
+
+console.log('\x00', '\xFF');
+
+console.log('\u0021', '\uFFFF');
+
+console.log('\u{20331}', '\u{1F60D}');
