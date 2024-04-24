03d6cf0b20da4d00ac2170fa12b9e44dc3bec0b8
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-24 22:13:10 +0300

    DOM tree

---
+++ src/browser/document/domTree.js
@@ -0,0 +1,8 @@
+// DOM tree
+document.body.style.background = 'purple';
+setTimeout(() => {
+	document.body.style.background = '';
+}, 3000);
+
+
+document.body.innerHTML = '<table id="table"><tr><td>1</td></tr></table>';
