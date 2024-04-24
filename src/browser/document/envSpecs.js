78104b18d89f22a8eaf2bc1355892e1f1d0de9f4
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-24 21:36:35 +0300

    Browser environment, specs

---
+++ src/browser/document/envSpecs.js
@@ -0,0 +1,28 @@
+// Browser environment, specs
+for (let key in window) {
+	//console.log(key);
+}
+
+for (let key in document) {
+	//console.log(key);
+}
+
+for (let key in navigator) {
+	//console.log(key, navigator[key]);
+}
+
+for (let key in screen) {
+	//console.log(key, screen[key]);
+}
+
+for (let key in location) {
+	//console.log(key, location[key]);
+}
+
+for (let key in frames) {
+	//console.log(key, frames[key]);
+}
+
+for (let key in history) {
+	//console.log(key, history[key]);
+}
