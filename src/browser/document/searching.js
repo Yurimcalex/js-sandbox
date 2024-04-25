637b751b5cd2aea1acc7a06b0890a83198203ed1
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-04-25 21:12:42 +0300

    Searching: document.getElementById or id

---
+++ src/browser/document/searching.js
@@ -0,0 +1,5 @@
+// Searching: getElement*, querySelector*
+// document.getElementById or id
+let box = document.getElementById('box');
+box.style.background = 'lightgray';
+window['box-content'].style.color = 'gray';
