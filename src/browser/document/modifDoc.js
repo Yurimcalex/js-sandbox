36af710ef9ea3a54c6be64ee9d1b2c6088a0e4bc
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-05 19:53:29 +0300

    Insertion methods

--- src/browser/document/modifDoc.js
+++ src/browser/document/modifDoc.js
@@ -5,3 +5,22 @@ let textNode = document.createTextNode('Here I am');
 let div1 = document.createElement('div');
 div1.className = 'alert';
 div1.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
+
+// Insertion methods
+document.body.append(div1);
+
+ol.before('before');
+ol.after('after');
+
+let liFirst = document.createElement('li');
+liFirst.innerHTML = 'prepend';
+ol.prepend(liFirst);
+
+let liLast = document.createElement('li');
+liLast.innerHTML = 'append';
+ol.append(liLast);
+
+div1.after(
+	'<p>It will be text only</p>',
+	document.createElement('hr')
+);
