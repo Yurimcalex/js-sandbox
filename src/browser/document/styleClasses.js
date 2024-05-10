eb644004ca3055bfee6ddfdfff384c9a637b9240
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-10 22:20:44 +0300

    Style and classes - className and classList

---
+++ src/browser/document/styleClasses.js
@@ -0,0 +1,19 @@
+// Styles and classes
+console.log(div1.className);
+div1.className = 'replace all previous';
+console.log(div1.className);
+div1.className = '';
+
+div1.classList.add('container');
+div1.classList.add('full_width');
+div1.classList.add('green');
+
+console.log(div1.classList.contains('green'));
+div1.classList.toggle('green');
+console.log(div1.classList.contains('green'));
+
+div1.classList.remove('full_width');
+
+for (let cls of div1.classList) {
+	console.log(cls);
+}
