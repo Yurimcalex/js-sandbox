b202bd10e4a25ebbc7cb6a6c6a2c5f4376f6de3c
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-22 20:30:16 +0300

    Using for fixed position

--- src/browser/document/coordinates.js
+++ src/browser/document/coordinates.js
@@ -11,3 +11,25 @@ let centerY = document.documentElement.clientHeight / 2;
 let elem = document.elementFromPoint(centerX, centerY);
 
 console.log(elem);
+
+
+// Using for fixed position
+function createMessageUnder(elem, html) {
+	let message = document.createElement('div');
+	message.style.cssText = `
+		position: fixed;
+		color: red;
+	`;
+
+	let coords = elem.getBoundingClientRect();
+
+	message.style.left = coords.left + 'px';
+	message.style.top = coords.bottom + 'px';
+
+	message.innerHTML = html;
+	return message;
+}
+
+let message = createMessageUnder(example, 'Some message under...');
+document.body.append(message);
+setTimeout(() => message.remove(), 5000);
