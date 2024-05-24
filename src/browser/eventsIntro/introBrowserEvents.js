bc2eae9eaf76e8890ce4e1a771e5726bdb4528cb
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-24 16:23:50 +0300

    Object handlers: handleEvent

--- src/browser/eventsIntro/introBrowserEvents.js
+++ src/browser/eventsIntro/introBrowserEvents.js
@@ -32,3 +32,44 @@ btn2.onclick = function (e) {
 	console.log(e.type + ' at ' + e.currentTarget);
 	console.log('Coords: ' + e.clientX + ',' + e.clientY);
 };
+
+
+// Object handlers: handleEvent
+let obj = {
+	handleEvent(e) {
+		console.log(e.type + ' at ' + e.currentTarget + ' : handler3')
+	}
+};
+
+btn2.addEventListener('click', obj);
+
+class Menu {
+	handleEvent(e) {
+		switch (e.type) {
+			case 'mousedown':
+				btn3.innerHTML = 'Mouse button pressed';
+				break;
+			case 'mouseup':
+				btn3.innerHTML += '... and releases';
+				break;
+		}
+	}
+}
+
+let menu = new Menu();
+btn3.addEventListener('mousedown', menu);
+btn3.addEventListener('mouseup', menu);
+
+
+class Menu_1 {
+	handleEvent(e) {
+		let method = `on${e.type[0].toUpperCase()}${e.type.slice(1)}`;
+		this[method](e);
+	}
+
+	onClick(e) {
+		console.log('Menu clicked!');
+	}
+}
+
+btn4.addEventListener('click', new Menu_1());
