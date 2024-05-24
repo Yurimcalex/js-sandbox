f6dbf59e1aec20bb0012fc76ca1a1b4d6ed8ea48
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-24 14:00:31 +0300

    addEventListener

--- src/browser/eventsIntro/introBrowserEvents.js
+++ src/browser/eventsIntro/introBrowserEvents.js
@@ -8,3 +8,20 @@ btn1.onclick = null;
 btn1.onclick = function () {
 	console.log(this.innerHTML);
 }
+
+
+// addEventListener
+function handler1() {
+	console.log('handler1');
+}
+
+function handler2() {
+	console.log('handler2');
+}
+
+btn1.addEventListener('click', handler1);
+btn1.addEventListener('click', handler2);
+
+document.addEventListener('DOMContentLoaded', function () {
+	console.log('DOM built!');
+});
