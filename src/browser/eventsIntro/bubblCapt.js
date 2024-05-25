25ccce24136281785050ad8eeadc48e61a17284f
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-25 17:08:38 +0300

    Bubbling

---
+++ src/browser/eventsIntro/bubblCapt.js
@@ -0,0 +1,26 @@
+// Bubbling and capturing
+div.onclick = function () {
+	console.log('I am the div handler!')
+}
+
+let conts = [level1, level2, level3];
+conts.forEach(elem => {
+	elem.addEventListener('click', function (e) {
+		//console.log(this.tagName);
+	});
+});
+
+// event.target
+level3.addEventListener('click', function (e) {
+	console.log('target', e.target);
+	console.log('owner', e.currentTarget);
+});
+
+document.body.onclick = function () {
+	console.log('BODY');
+};
+
+level1.addEventListener('click', function (e) {
+	console.log('fires on', e.target);
+	e.stopPropagation();
+});
