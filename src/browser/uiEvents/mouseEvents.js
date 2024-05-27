f907230d0a2fe99bffa83bb3c352d7bb5df9aa6c
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-27 19:35:14 +0300

    Coordinates: clientX/Y, pageX/Y

--- src/browser/uiEvents/mouseEvents.js
+++ src/browser/uiEvents/mouseEvents.js
@@ -4,3 +4,14 @@ button1.onclick = function (e) {
 		console.log('All right!');
 	}
 };
+
+
+// Coordinates: clientX/Y, pageX/Y
+document.onmousemove = function (e) {
+	let html = `
+		window coords: x:${e.clientX}, y:${e.clientY}<br/>
+		page coords: x:${e.pageX}, y:${e.pageY}
+	`;
+
+	coords.innerHTML = html;
+};
