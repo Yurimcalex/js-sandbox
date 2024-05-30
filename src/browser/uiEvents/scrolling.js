12fef15ee3e56aa780ad522d40f706e5d3af5d2c
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-30 19:52:27 +0300

    Task 2 - Up/down button

--- src/browser/uiEvents/scrolling.js
+++ src/browser/uiEvents/scrolling.js
@@ -9,9 +9,23 @@ section.addEventListener('scroll', function (e) {
 	let currScroll = section.scrollTop;
 	let fullScroll = section.scrollHeight - section.clientHeight;
 	if (currScroll > fullScroll - 100) {
-		section.append(section.children[0].cloneNode(true));
-	} 
+		section.append(section.children[1].cloneNode(true));
+	}
+
+	toTop.style.top = currScroll + 5 + 'px';
+
+	if (currScroll > section.clientHeight) {
+		toTop.hidden = false;	
+	}
 });
 
 console.log(document.documentElement.getBoundingClientRect());
 console.log(document.documentElement.clientHeight);
+
+
+// Task 2 - Up/down button
+toTop.onclick = function (e) {
+	e.preventDefault();
+	section.scrollTop = 0;
+	toTop.hidden = true;
+};
