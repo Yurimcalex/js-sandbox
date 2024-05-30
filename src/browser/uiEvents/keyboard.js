2c6f2e11a840e4a5de8594daac286a533bedfaf2
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-30 18:16:05 +0300

    Task 1 - Extended hotkeys

--- src/browser/uiEvents/keyboard.js
+++ src/browser/uiEvents/keyboard.js
@@ -9,3 +9,36 @@ phone.onkeydown = function (e) {
 	return (e.key >= '0' && e.key <= '9') ||
 		['+', '(', ')', '-', 'ArrowLeft','ArrowRight','Delete','Backspace'].includes(e.key);
 };
+
+
+// Task 1 - Extended hotkeys
+function runOnKeys(func, ...codes) {
+	let pressedKeys = new Set();
+
+	function onKeyDown(e) {
+		if (codes.includes(e.key)) {
+			pressedKeys.add(e.key);
+		}
+
+		if (pressedKeys.size === codes.length) {
+			func();
+			pressedKeys.clear();
+		}
+	}
+
+	function onKeyUp(e) {
+		if (pressedKeys.has(e.key)) {
+			pressedKeys.delete(e.key);
+		}
+	}
+
+	document.addEventListener('keydown', onKeyDown);
+	document.addEventListener('keyup', onKeyUp);
+
+	return () => {
+		document.removeEventListener('keydown', onKeyDown);
+		document.removeEventListener('keyup', onKeyUp);
+	};
+}
+
+runOnKeys(() => alert('ok'), 'a', 's');
