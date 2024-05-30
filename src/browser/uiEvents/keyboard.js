3d4f988577d485724d4c1fc98eb2518064ac51fb
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-30 17:41:35 +0300

    Default actions

--- src/browser/uiEvents/keyboard.js
+++ src/browser/uiEvents/keyboard.js
@@ -2,3 +2,10 @@
 document.addEventListener('keydown', function (e) {
 	console.log(`key:${e.key} keyCode:${e.code}`);
 });
+
+
+// Default actions
+phone.onkeydown = function (e) {
+	return (e.key >= '0' && e.key <= '9') ||
+		['+', '(', ')', '-', 'ArrowLeft','ArrowRight','Delete','Backspace'].includes(e.key);
+};
