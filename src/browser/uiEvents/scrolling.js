df77040ecec5fcb285e6480c55fd039d8d472491
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-05-30 19:33:57 +0300

    Task 1 - Endless page

--- src/browser/uiEvents/scrolling.js
+++ src/browser/uiEvents/scrolling.js
@@ -2,3 +2,16 @@
 window.addEventListener('scroll', function (e) {
 	scrollInfo.innerHTML = `Current scroll: ${window.pageYOffset}px`;
 });
+
+
+// Task 1 - Endless page
+section.addEventListener('scroll', function (e) {
+	let currScroll = section.scrollTop;
+	let fullScroll = section.scrollHeight - section.clientHeight;
+	if (currScroll > fullScroll - 100) {
+		section.append(section.children[0].cloneNode(true));
+	} 
+});
+
+console.log(document.documentElement.getBoundingClientRect());
+console.log(document.documentElement.clientHeight);
