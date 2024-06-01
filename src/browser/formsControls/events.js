75903cf6f76a74e56d5071a61893ec668a4e409c
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-01 19:55:53 +0300

    Event: input

--- src/browser/formsControls/events.js
+++ src/browser/formsControls/events.js
@@ -7,3 +7,9 @@ inp1.onchange = function () {
 sel1.onchange = function () {
 	console.log(this.value);
 };
+
+
+// Event: input
+inp2.oninput = function () {
+	inp2Result.innerHTML = inp2.value;
+};
