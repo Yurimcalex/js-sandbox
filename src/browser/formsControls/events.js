d6dc1e014e26644f28ccd102f9ccaac6a397e910
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-01 20:07:42 +0300

    Events: cut, copy, paste

--- src/browser/formsControls/events.js
+++ src/browser/formsControls/events.js
@@ -13,3 +13,15 @@ sel1.onchange = function () {
 inp2.oninput = function () {
 	inp2Result.innerHTML = inp2.value;
 };
+
+
+// Events: cut, copy, paste
+inp3.onpaste = function (e) {
+	console.log('paste: ' + e.clipboardData.getData('text/plain'));
+	e.preventDefault();
+};
+
+inp3.oncut = function (e) {
+	console.log(e.type + '-' + document.getSelection());
+	e.preventDefault();
+};
