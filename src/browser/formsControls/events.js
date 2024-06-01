7bb7e364fec2afe3a8eb5cde825b729e01b87657
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-01 19:39:00 +0300

    Event: change

---
+++ src/browser/formsControls/events.js
@@ -0,0 +1,9 @@
+// Events: change, input, cut, copy, paste
+// Event: change
+inp1.onchange = function () {
+	console.log(this.value);
+};
+
+sel1.onchange = function () {
+	console.log(this.value);
+};
