0708af19dfe35845a4ecd7dfb3d40bf416ccb946
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-01 20:38:24 +0300

    Task 1 - Deposit calculator

--- src/browser/formsControls/events.js
+++ src/browser/formsControls/events.js
@@ -25,3 +25,24 @@ inp3.oncut = function (e) {
 	console.log(e.type + '-' + document.getSelection());
 	e.preventDefault();
 };
+
+
+// Task 1 - Deposit calculator
+let form = document.forms.calculator;
+form.onchange = function (e) {
+	let initial = +form.money.value;
+	let years = +form.months.value / 12;
+	let interest = +form.interest.value / 100;
+
+	let result = Math.round(initial * (1 + interest) ** years);
+
+	window['money-before'].textContent = initial;
+	window['money-after'].textContent = Math.round(result);
+	
+	let h = result.toString().slice(0, 3);
+	while (h.length < 3) {
+		h += '0';
+	}
+
+	window['height-after'].style.height = h + 'px';
+}
