4c3a1cd52269f7ed450e5b906d715df17f79ea73
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-01 16:02:06 +0300

    Task 2 - Edit TD on click

--- src/browser/formsControls/focusing.js
+++ src/browser/formsControls/focusing.js
@@ -76,3 +76,60 @@ function ced() {
 }
 
 ced();
+
+
+// Task 2 - Edit TD on click
+let tbl = document.getElementById('bagua-table');
+let isEditing = false;
+tbl.onclick = function (e) {
+	if (isEditing) return;
+	isEditing = true;
+	let td = e.target.closest('td');
+	if (td && tbl.contains(td)) {
+		td.style.position = 'relative';
+		let [container, textarea, okBtn, cancelBtn] =
+			createEditingArea(td.clientWidth, td.clientHeight, td.innerHTML);
+		td.append(container);
+		textarea.focus();
+
+		okBtn.onclick = onInput;
+		cancelBtn.onclick = onInput;
+
+		function onInput() {
+			td.innerHTML = textarea.value;
+			container.remove();
+			isEditing = false;
+		}
+	}
+}
+
+function createEditingArea(width, height, content) {
+	let area = document.createElement('div');
+	area.style.cssText = `
+		position: absolute;
+		top: 0;
+		left: 0;
+	`;
+
+	let text = document.createElement('textarea');
+	text.style.cssText = `
+		display: block;
+		height: ${height}px;
+		width: ${width}px;
+		padding: 0;
+		outline: none;
+		border: none;
+		resize: none;
+	`;
+	text.value = content;
+
+	let ok = document.createElement('button');
+	ok.textContent = 'Ok';
+
+	let cancel = document.createElement('button');
+	cancel.textContent = 'Cancel';
+
+	area.append(text, ok, cancel);
+
+	return [area, text, ok, cancel];
+} 
