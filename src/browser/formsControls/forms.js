ecdfcc0e10f7f1950e75e527cda4e888e60bddd0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-02 15:32:31 +0300

    Task 1 - Modal form

--- src/browser/formsControls/forms.js
+++ src/browser/formsControls/forms.js
@@ -10,7 +10,7 @@ form1.elements[1].onclick = function () {
 };
 
 
-// Methid: submit
+// Method: submit
 let form = document.createElement('form');
 form.action = 'https://google.com/search';
 form.method = 'GET';
@@ -20,3 +20,67 @@ document.body.append(form);
 setTimeout(() => {
 	//form.submit();
 }, 3000);
+
+
+// Task 1 - Modal form
+function showPrompt(html, callback) {
+	let container = document.createElement('div');
+	container.className = 'modal-container';
+	let form = document.createElement('form');
+	form.className = 'modal-form';
+	form.innerHTML = `
+		${html}
+		<br/>
+		<input type="text" name="text">
+		<br/>
+		<input type="submit" name="ok" value="Ok">
+		<input type="button" name="cancel" value="Cancel">
+	`;
+
+	document.body.append(form);
+	document.body.classList.add('hideOverflow');
+	document.body.append(container);
+
+	function clear() {
+		container.remove();
+		form.remove();
+		document.body.classList.remove('hideOverflow');
+	}
+
+	let text = form.elements.text;
+	let cancel = form.elements.cancel;
+
+	text.focus();
+	form.onsubmit = function () {
+		let inp = text.value;
+		if (!inp) {
+			return false;
+		}
+		callback(inp);
+		clear();
+		return false;
+	};
+
+	cancel.onclick = function () {
+		callback(null);
+		clear();
+	};
+
+	text.onkeydown = function (e) {
+		if (e.code === 'Tab') {
+			cancel.focus();
+			return false;
+		}
+	};
+
+	cancel.onkeydown = function (e) {
+		if (e.code === 'Tab') {
+			text.focus();
+			return false;
+		}
+	};
+}
+
+showPrompt('Enter something<br>...smart :)', function (value) {
+	console.log(value);
+});
