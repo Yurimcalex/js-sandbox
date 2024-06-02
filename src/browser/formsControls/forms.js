c6675281f7757c51726d98015a1bcec632aa0e69
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-02 14:41:01 +0300

    Method submit

--- src/browser/formsControls/forms.js
+++ src/browser/formsControls/forms.js
@@ -8,3 +8,15 @@ form1.onsubmit = function () {
 form1.elements[1].onclick = function () {
 	console.log('Click!');
 };
+
+
+// Methid: submit
+let form = document.createElement('form');
+form.action = 'https://google.com/search';
+form.method = 'GET';
+form.innerHTML = '<input name="q" value="test">';
+document.body.append(form);
+
+setTimeout(() => {
+	//form.submit();
+}, 3000);
