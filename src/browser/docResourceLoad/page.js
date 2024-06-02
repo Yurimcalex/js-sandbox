26abcd80b3c775f1afac9cf680226d589ba73762
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-02 16:10:46 +0300

    Page: DOMContentLoaded

---
+++ src/browser/docResourceLoad/page.js
@@ -0,0 +1,6 @@
+// Page: DOMContentLoaded, load, beforeunload, unload
+// DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function () {
+	console.log('DOM is ready!');
+	console.log(`Image size: ${img.offsetWidth}x${img.offsetHeight}`);
+});
