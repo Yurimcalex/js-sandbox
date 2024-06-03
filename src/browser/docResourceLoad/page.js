7acd4985b19671c84a40adcfd371fc1e09aae839
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-03 18:39:32 +0300

    DOMContentLoaded and scripts

--- src/browser/docResourceLoad/page.js
+++ src/browser/docResourceLoad/page.js
@@ -4,3 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
 	console.log('DOM is ready!');
 	console.log(`Image size: ${img.offsetWidth}x${img.offsetHeight}`);
 });
+
+
+// DOMContentLoaded and scripts
+let script = document.createElement('script');
+script.innerHTML = 'console.log("generated!")';
+document.body.append(script);
