e7cd4794c0a681165b1c8c8e1d4e6a32541209f9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-04 18:36:59 +0300

    Loading a script - script.onerror

--- src/browser/docResourceLoad/resourceLoading.js
+++ src/browser/docResourceLoad/resourceLoading.js
@@ -7,3 +7,10 @@ document.head.append(script);
 script.onload = function () {
 	console.log(_.VERSION);
 };
+
+
+let script1 = document.createElement('script');
+script1.src = "https://somesite.com/404.js";
+script1.onerror = function () {
+	console.log('Error loading' + this.src);
+};
