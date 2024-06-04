17ce4d913a9a0050aa21814afe169daadb72d225
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-04 19:16:58 +0300

    Task 1 - Load images with a callback

--- src/browser/docResourceLoad/resourceLoading.js
+++ src/browser/docResourceLoad/resourceLoading.js
@@ -31,3 +31,37 @@ img.onerror = function () {
 window.onerror = function (message, url, line, col, errorObj) {
 	console.log(`${message}\n${url}, ${line}:${col}`);
 };
+
+
+
+// Task 1 - Load images with a callback
+function preloadImages(sources, callback) {
+	Promise.all(sources.map(src => {
+		return new Promise((resolve, reject) => {
+			let img = document.createElement('img');
+			img.src = src;
+			img.onload = function () {
+				resolve(img);
+			};
+			img.onerror = function () {
+				reject(new Error('img loading failed'));
+			};
+		});
+	}))
+	.then(callback)
+	.catch(err => console.log(err));
+}
+
+function loaded() {
+  console.log("Images loaded");
+}
+
+let sources = [
+  "https://en.js.cx/images-load/1.jpg",
+  "https://en.js.cx/images-load/2.jpg",
+  "https://en.js.cx/images-load/3.jpg"
+];
+
+preloadImages(["1.jpg", "2.jpg", "3.jpg"], loaded);
+
+preloadImages(sources, loaded);
