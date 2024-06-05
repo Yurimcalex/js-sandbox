a48b512e93b259cdd0c0f0bc77246203b5a94207
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-05 17:13:48 +0300

    Selecting element nodes

--- src/browser/mics/selectionRange.js
+++ src/browser/mics/selectionRange.js
@@ -4,3 +4,11 @@ let range = new Range();
 range.setStart(text.firstChild, 0);
 range.setEnd(text.firstChild, 4);
 console.log(range.toString());
+
+
+// Selecting element nodes
+let pRange = new Range();
+pRange.setStart(p, 0);
+pRange.setEnd(p, 2);
+console.log(pRange.toString());
+document.getSelection().addRange(pRange);
