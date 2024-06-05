150ed3377397444ada8b18090c353c25e3767a8b
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-05 16:37:44 +0300

    Dynamic highlight demo

--- src/browser/mics/mutationObserver.js
+++ src/browser/mics/mutationObserver.js
@@ -8,3 +8,38 @@ observer.observe(elem, {
 	subtree: true,
 	characterDataOldValue: true
 });
+
+
+// Dynamic highlight demo
+let observer1 = new MutationObserver(mutations => {
+	for (let mutation of mutations) {
+		for (let node of mutation.addedNodes) {
+			if (!(node instanceof HTMLElement)) continue;
+			
+			if (node.matches('p.important')) {
+				node.style.color = 'blue';
+			}
+
+			for (let elem of node.querySelectorAll('p.important')) {
+				elem.style.color = 'blue';
+			}
+		}
+	}
+});
+
+
+let container = document.getElementById('container');
+observer1.observe(container, {
+	childList: true,
+	subtree: true
+});
+
+
+setTimeout(() => {
+	let div = document.createElement('div');
+	div.innerHTML = `
+		<p class="important">Some important info ...</p>
+		<p>What is this?</p>
+	`;
+	container.append(div);
+}, 5000);
