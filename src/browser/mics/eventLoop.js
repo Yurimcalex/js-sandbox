101520645a40dd9a464f3aed605a1f2100c934ed
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-09 20:35:00 +0300

    Task 1 - What will be the output of this code

--- src/browser/mics/eventLoop.js
+++ src/browser/mics/eventLoop.js
@@ -85,3 +85,21 @@ Promise.resolve().then(() => console.log('promise'));
 console.log('code');
 
 console.log(queueMicrotask);
+
+
+// Task 1 - What will be the output of this code
+console.log(1);
+
+setTimeout(() => console.log(2));
+
+Promise.resolve().then(() => console.log(3));
+
+Promise.resolve().then(() => setTimeout(() => console.log(4)));
+
+Promise.resolve().then(() => console.log(5));
+
+setTimeout(() => console.log(6));
+
+console.log(7);
+
+// 1 7 3 5 2 6 4
