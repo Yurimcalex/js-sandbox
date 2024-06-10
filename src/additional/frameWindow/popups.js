67303ba94b03ecc0e481cc97eefbdcf0ecd783eb
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-10 19:39:03 +0300

    Example: a minimalistic window

--- src/additional/frameWindow/popups.js
+++ src/additional/frameWindow/popups.js
@@ -5,3 +5,35 @@
 popup.onclick = () => {
 	window.open('https://google.com');
 };
+
+
+// Example: a minimalistic window
+let params = [
+	'scrollbar=no',
+	'resizable=no',
+	'status=no',
+	'location=no',
+	'toolbar=no',
+	'menubar=no',
+	'width=0',
+	'height=0',
+	'left=-1000',
+	'top=-1000'
+];
+
+let params1 = [
+	'scrollbar=no',
+	'resizable=no',
+	'status=no',
+	'location=no',
+	'toolbar=no',
+	'menubar=no',
+	'width=600',
+	'height=300',
+	'left=100',
+	'top=100'
+];
+
+openw.onclick = () => {
+	open('/', 'test', params1);
+};
