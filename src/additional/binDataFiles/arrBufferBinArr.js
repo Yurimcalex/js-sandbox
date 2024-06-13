fc7b3b179a03efc03edb2ac499f272e7b01a92e3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-13 15:43:14 +0300

    ArrayBuffer, binary arrays

---
+++ src/additional/binDataFiles/arrBufferBinArr.js
@@ -0,0 +1,10 @@
+// ArrayBuffer, binary arrays
+let buffer = new ArrayBuffer(16);
+console.log(buffer.byteLength);
+
+let view = new Uint32Array(buffer);
+
+console.log(Uint32Array.BYTES_PER_ELEMENT);
+
+console.log(view.length);
+console.log(view.byteLength);
