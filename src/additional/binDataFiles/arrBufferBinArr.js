2732c35335089023d0bbde876be854203ae069e1
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-13 18:43:57 +0300

    DataView

--- src/additional/binDataFiles/arrBufferBinArr.js
+++ src/additional/binDataFiles/arrBufferBinArr.js
@@ -52,3 +52,18 @@ console.log(nw);
 
 nw.set([10, 9]);
 console.log(nw);
+
+
+// DataView
+let buff = new Uint8Array([255, 255, 255, 255]).buffer;
+console.log(buff);
+let dataView = new DataView(buff);
+console.log(dataView);
+
+console.log(dataView.getUint8(0));
+console.log(dataView.getUint16(0));
+console.log(dataView.getUint32(0));
+
+dataView.setUint32(0, 0);
+
+console.log(dataView.getUint8(0));
