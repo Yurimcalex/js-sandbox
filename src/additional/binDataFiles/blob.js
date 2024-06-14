eef8a4beb38dcc31936fbf1536d7b088426abf0e
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-14 13:25:52 +0300

    Blob as URL

--- src/additional/binDataFiles/blob.js
+++ src/additional/binDataFiles/blob.js
@@ -6,3 +6,18 @@ let hello = new Uint8Array([72, 101, 108, 108, 111]);
 let b = new Blob([hello, ' ', 'world'], {type: 'text/plain'});
 
 console.log(b);
+
+
+// Blob as URL
+let blob1 = new Blob(['Hello, World!'], {type: 'text/plain'});
+
+link.href = URL.createObjectURL(blob1);
+
+{
+	let link = document.createElement('a');
+	link.download = 'hello.txt';
+	let blob = new Blob(['Hello, everyone!'], {type: 'text/plain'});
+	link.href = URL.createObjectURL(blob);
+	//link.click();
+	URL.revokeObjectURL(link.href);
+}
