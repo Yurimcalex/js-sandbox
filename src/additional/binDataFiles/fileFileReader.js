f425d591d8de2cd99ba256b77b955501ef396734
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-14 15:58:36 +0300

    FileReader

--- src/additional/binDataFiles/fileFileReader.js
+++ src/additional/binDataFiles/fileFileReader.js
@@ -4,3 +4,20 @@ inp.onchange = function () {
 	console.log(`File name: ${file.name}`);
 	console.log(`Last modified: ${file.lastModified}`);
 }
+
+
+// FileReader
+inp.addEventListener('change', function () {
+	let file = inp.files[0];
+	let reader = new FileReader();
+
+	reader.readAsText(file);
+
+	reader.onload = function () {
+		console.log(reader.result);
+	};
+
+	reader.onerror = function () {
+		console.log(reader.error);
+	};
+});
