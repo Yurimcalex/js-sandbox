4cea9a2d05a79c54a9ddc05f5675d9088671c20a
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-16 19:19:30 +0300

    Using with fetch

--- src/additional/networkRequests/fetchAbort.js
+++ src/additional/networkRequests/fetchAbort.js
@@ -7,3 +7,25 @@ setTimeout(() => {
 	controller.abort();
 	console.log(signal.aborted)
 }, 3000);
+
+
+// Using with fetch
+async function f() {
+	let controller = new AbortController();
+	setTimeout(() => controller.abort());
+
+	try {
+		let response = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
+			signal: controller.signal
+		});
+
+	} catch (e) {
+		if (e.name === 'AbortError') {
+			console.log('canceled fetch!');
+		} else {
+			throw e;
+		}
+	}
+}
+
+f();
