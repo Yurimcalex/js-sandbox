7cb3dda23bb554c3c120696ddd7a13c3a2227fcb
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-19 19:57:28 +0300

    HTTP-headers

--- src/additional/networkRequests/XMLHttpRequest.js
+++ src/additional/networkRequests/XMLHttpRequest.js
@@ -71,17 +71,34 @@ xhr3.onprogress = function (e) {
 
 
 // Synchronous requests
-console.log('starting...');
-let xhr4 = new XMLHttpRequest();
-xhr4.open('GET', 'https://jsonplaceholder.typicode.com/users/1', false);
-try {
-	xhr4.send();
-	if (xhr4.status != 200) {
-		console.log(`Error ${xhr4.status}: ${xhr4.statusText}`)
-	} else {
-		console.log(xhr4.response);
-	}
-} catch (err) {
-	console.log('Request failed');
-}
-console.log('finishing...');
+// console.log('starting...');
+// let xhr4 = new XMLHttpRequest();
+// xhr4.open('GET', 'https://jsonplaceholder.typicode.com/users/1', false);
+// try {
+// 	xhr4.send();
+// 	if (xhr4.status != 200) {
+// 		console.log(`Error ${xhr4.status}: ${xhr4.statusText}`)
+// 	} else {
+// 		console.log(xhr4.response);
+// 	}
+// } catch (err) {
+// 	console.log('Request failed');
+// }
+// console.log('finishing...');
+
+
+// HTTP-headers
+let xhrPost = new XMLHttpRequest();
+xhrPost.open('POST', 'https://jsonplaceholder.typicode.com/posts');
+xhrPost.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+xhrPost.send(JSON.stringify({
+  title: 'foo',
+  body: 'bar',
+  userId: 1,
+}));
+
+xhrPost.onload = function () {
+	xhrPost.getAllResponseHeaders()
+		.split('\r\n')
+		.forEach(h => console.log(h));
+};
