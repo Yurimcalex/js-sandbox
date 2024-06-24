ac0cd1d22c3f7f5c9b1c8d6c5b1bb309dcdefdc9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-24 17:15:41 +0300

    Appendix: Cookie functions

--- src/additional/storingData/cookies.js
+++ src/additional/storingData/cookies.js
@@ -15,3 +15,51 @@ console.log(date);
 
 document.cookie = 'expires=' + date;
 console.log(document.cookie);
+
+
+// Appendix: Cookie functions
+function getCookie(name) {
+	let matches = document.cookie.match(
+		new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)")
+	);
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+let expires = getCookie('expires');
+console.log(expires);
+
+
+function setCookie(name, value, attributes = {}) {
+	attributes = {
+		path: '/',
+		...attributes
+	};
+
+	if (attributes.expires instanceof Date) {
+		attributes.expires = attributes.expires.toUTCString();
+	}
+
+	let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+	for (let attributeKey in attributes) {
+		updatedCookie += '; ' + attributeKey;
+		let attributeValue = attributes[attributeKey];
+		if (attributeValue !== true) {
+			updatedCookie += '=' + attributeValue;
+		}
+	}
+
+	document.cookie = updatedCookie;
+}
+
+setCookie('user', 'Bob', {secure: true, 'max-age': 3600});
+
+console.log(document.cookie);
+
+
+function deleteCookie(name) {
+	setCookie(name, '', { 'max-age': -1 });
+}
+
+deleteCookie('name');
+console.log(document.cookie);
