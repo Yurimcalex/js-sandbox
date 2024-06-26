f32258d29d6cf14f9977863eacd47f279a9cb865
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-06-26 19:56:45 +0300

    localStorage

---
+++ src/additional/storingData/localSessionStorage.js
@@ -0,0 +1,33 @@
+// LocalStorage, sessionStorage
+// local Storage demo
+localStorage.setItem('fruit', 'banana');
+console.log(localStorage.getItem('fruit'));
+
+
+// Object-like access
+localStorage.sport = 'football';
+console.log(localStorage.sport);
+delete localStorage.sport;
+
+
+// Looping over keys
+for (let i = 0; i < localStorage.length; i += 1) {
+	let key = localStorage.key(i);
+	console.log(`${key}: ${localStorage.getItem(key)}`);
+}
+
+let keys = Object.keys(localStorage);
+for (let key of keys) {
+	console.log(key, localStorage.getItem(key));
+}
+
+
+// Strings only
+localStorage.user = { name: 'Bob' };
+console.log(localStorage.user);
+
+localStorage.user = JSON.stringify({ name: 'Bob' });
+let user = JSON.parse(localStorage.user);
+console.log(user);
+
+console.log(JSON.stringify(localStorage, null, 2));
