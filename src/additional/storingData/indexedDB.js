13bb29133f3e5910962a06e58811ea7f502f68f7
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-07-03 17:31:19 +0300

    Parallel update problem

--- src/additional/storingData/indexedDB.js
+++ src/additional/storingData/indexedDB.js
@@ -1,22 +1,41 @@
 // IndexedDB
 // Open database
+// let openRequest = indexedDB.open('store', 1);
+
+// openRequest.onupgradeneeded = function () {
+// 	console.log('upgrade!');
+// };
+
+// openRequest.onerror = function () {
+// 	console.log('Error', openRequest.error);
+// };
+
+// openRequest.onsuccess = function () {
+// 	let db = openRequest.result;
+
+// 	console.log(db);
+// };
+
+
+// let openRequest1 = indexedDB.open('store', 2);
+
+// openRequest1.onupgradeneeded = function (event) {
+// 	let db = openRequest.result;
+
+// 	switch (event.oldVersion) {
+// 		case 0:
+// 			console.log('database store initialized!');
+// 		case 1:
+// 			console.log('database store updated to version 2');
+// 	}
+// };
+
+
+// let deleteRequest = indexedDB.deleteDatabase('store');
+
+
+// Parallel update problem
 let openRequest = indexedDB.open('store', 1);
-
-openRequest.onupgradeneeded = function () {
-	console.log('upgrade!');
-};
-
-openRequest.onerror = function () {
-	console.log('Error', openRequest.error);
-};
-
-openRequest.onsuccess = function () {
-	let db = openRequest.result;
-
-	console.log(db);
-};
-
-
 let openRequest1 = indexedDB.open('store', 2);
 
 openRequest1.onupgradeneeded = function (event) {
@@ -30,5 +49,19 @@ openRequest1.onupgradeneeded = function (event) {
 	}
 };
 
+openRequest.onerror = function () {
+	console.log('Error', openRequest.error);
+};
 
-let deleteRequest = indexedDB.deleteDatabase('store');
+openRequest.onsuccess = function () {
+	let db = openRequest.result;
+
+	db.onversionchange = function () {
+		db.close();
+		console.log('Database is outdated, please reload the page.');
+	};
+};
+
+openRequest.onblocked = function () {
+	console.log('blocked!');
+};
