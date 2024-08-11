b2544d9d7eab62bc4eeccb7a3a3033d2d5968e74
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-08-11 14:44:32 +0300

    indexedDb - object store

--- src/additional/storingData/indexedDB.js
+++ src/additional/storingData/indexedDB.js
@@ -35,33 +35,61 @@
 
 
 // Parallel update problem
-let openRequest = indexedDB.open('store', 1);
-let openRequest1 = indexedDB.open('store', 2);
+// let openRequest = indexedDB.open('store', 1);
+// let openRequest1 = indexedDB.open('store', 2);
 
-openRequest1.onupgradeneeded = function (event) {
-	let db = openRequest.result;
+// openRequest1.onupgradeneeded = function (event) {
+// 	let db = openRequest.result;
 
-	switch (event.oldVersion) {
-		case 0:
-			console.log('database store initialized!');
-		case 1:
-			console.log('database store updated to version 2');
+// 	switch (event.oldVersion) {
+// 		case 0:
+// 			console.log('database store initialized!');
+// 		case 1:
+// 			console.log('database store updated to version 2');
+// 	}
+// };
+
+// openRequest.onerror = function () {
+// 	console.log('Error', openRequest.error);
+// };
+
+// openRequest.onsuccess = function () {
+// 	let db = openRequest.result;
+
+// 	db.onversionchange = function () {
+// 		db.close();
+// 		console.log('Database is outdated, please reload the page.');
+// 	};
+// };
+
+// openRequest.onblocked = function () {
+// 	console.log('blocked!');
+// };
+
+indexedDB.deleteDatabase('store');
+
+
+// Object store
+let or = indexedDB.open('store', 1);
+or.onupgradeneeded = function (e) {
+	console.log('upgrade!');
+	console.log(e.oldVersion);
+
+	let db = or.result;
+	if (!db.objectStoreNames.contains('skills')) {
+		db.createObjectStore('skills', {keyPath: 'id'});
 	}
+
+	console.log(db.objectStoreNames);
 };
 
-openRequest.onerror = function () {
-	console.log('Error', openRequest.error);
+
+or.onerror = function () {
+	console.log('Error', or.error);	
 };
 
-openRequest.onsuccess = function () {
-	let db = openRequest.result;
 
-	db.onversionchange = function () {
-		db.close();
-		console.log('Database is outdated, please reload the page.');
-	};
-};
-
-openRequest.onblocked = function () {
-	console.log('blocked!');
+or.onsuccess = function () {
+	let db = or.result;
+	console.log('success!');
 };
