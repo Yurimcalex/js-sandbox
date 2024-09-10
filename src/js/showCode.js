dc48acbeaf61fdfb299007ea6f7207b93352b591
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-09-10 13:47:44 +0300

    Create block of code creation fn

--- src/js/showCode.js
+++ src/js/showCode.js
@@ -2,3 +2,12 @@ async function getScriptContent() {
 	const script = document.querySelector('[data-code]');
 	return await fetch(script.src).then((res) => res.text());
 }
+
+async function createCodeBlock() {
+	const pre = document.createElement('pre');
+	const code = document.createElement('code');
+	code.className = 'language-javascript';
+	pre.append(code);
+	code.innerHTML = await getScriptContent();
+	document.body.append(pre);
+}
