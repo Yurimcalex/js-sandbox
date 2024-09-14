ccfb49b2dae59eca46fa7100b4b3bd9bf13d4467
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-09-14 13:40:21 +0300

    showCode clean up script

--- src/js/showCode.js
+++ src/js/showCode.js
@@ -1,25 +1,3 @@
-// async function getScriptContent() {
-// 	const script = document.querySelector('[data-code]');
-// 	return await fetch(script.src).then((res) => res.text());
-// }
-
-// async function createCodeBlock() {
-// 	const pre = document.createElement('pre');
-// 	const code = document.createElement('code');
-// 	code.className = 'language-javascript';
-// 	pre.append(code);
-// 	code.innerHTML = await getScriptContent();
-// 	document.body.append(pre);
-// }
-
-// document.addEventListener('DOMContentLoaded', async (event) => {
-// 	await createCodeBlock();
-//   document.querySelectorAll('pre code').forEach((block) => {
-//     hljs.highlightElement(block);
-//   });
-// });
-
-
 async function getScriptContent() {
 	const script = document.querySelector('[data-code]');
 	return await fetch(script.src).then((res) => res.text());
