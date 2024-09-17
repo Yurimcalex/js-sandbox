4de833c59be535a150b3481f359eb7381517cc23
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-09-17 21:50:45 +0300

    Create get console log line fn

--- src/js/showLogs.js
+++ src/js/showLogs.js
@@ -8,48 +8,60 @@ console = new Proxy(oldConsole, {
 
 		return new Proxy(oldConsole.log, {
 			apply: (target, thisArg, args) => {
-				store.set(args.join(', '), args);
+				const key = args.join(', ');
+				store.set(key, args);
 				target.apply(thisArg, args);
+				
 			}
 		});
 	}
 });
 
 
-function markLogs(text) {
-	let logsCounter = 1;
-	const logs = [];
+function getConsoleLogLine() {
+	try {
+		throw new Error();
+	} catch (err) {
+		const match = err.stack.split('\n')[1].match(/:\d*:/)[0];
+		return match.slice(1, match.length - 1);
+	}
+}
+
+
+// function markLogs(text) {
+// 	let logsCounter = 1;
+// 	const logs = [];
 	
-	const newText = text.replace(/console.log(.*)/g, str => {
-		logs.push([ logsCounter, getLogFromStore(str) ]);
-		return `[${logsCounter++}] ${str}`;
-	});
-	return [newText, logs];
-}
+// 	const newText = text.replace(/console.log(.*)/g, str => {
+// 		logs.push([ logsCounter, getLogFromStore(str) ]);
+// 		return `[${logsCounter++}] ${str}`;
+// 	});
+// 	return [newText, logs];
+// }
 
 
-function getLogFromStore(str) {
-	let argsStr = str.slice(12);
-	argsStr = argsStr.slice(0, argsStr.lastIndexOf(')'));
-	let args = eval(`[${argsStr}]`);
-	return store.get(args.join(', '));
-}
+// function getLogFromStore(str) {
+// 	let argsStr = str.slice(12);
+// 	argsStr = argsStr.slice(0, argsStr.lastIndexOf(')'));
+// 	let args = eval(`[${argsStr}]`);
+// 	return store.get(args.join(', '));
+// }
 
 
-function createLog(logsArr) {
-	const container = document.createElement('div');
-	container.className = 'log';
+// function createLog(logsArr) {
+// 	const container = document.createElement('div');
+// 	container.className = 'log';
 
-	logsArr.forEach((arr, ind) => 
-		container.innerHTML += `<div><span>log [${arr[0]}]:</span> ${arr[1] ? prepareArgs(arr[1]).join(', ') : '-'}</div>`)
+// 	logsArr.forEach((arr, ind) => 
+// 		container.innerHTML += `<div><span>log [${arr[0]}]:</span> ${arr[1] ? prepareArgs(arr[1]).join(', ') : '-'}</div>`)
 
-	document.body.append(container);
-}
+// 	document.body.append(container);
+// }
 
 
-function prepareArgs(args) {
-	return args.map(arg => {
-		if (typeof arg === 'object') return JSON.stringify(arg, null, 2);
-		return arg;
-	});
-}
+// function prepareArgs(args) {
+// 	return args.map(arg => {
+// 		if (typeof arg === 'object') return JSON.stringify(arg, null, 2);
+// 		return arg;
+// 	});
+// }
