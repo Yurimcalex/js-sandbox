85a10389ff4dc559bbdfbefbe21db4a1aba83273
Yuri Sklyarov <yuri.mcalex@gmail.com>
2024-09-18 14:27:25 +0300

    Update symbol page

--- src/lang/objects/symbol/symbol.js
+++ src/lang/objects/symbol/symbol.js
@@ -1,8 +1,8 @@
 let id1 = Symbol('id');
 let id2 = Symbol('id');
-//console.log(id1 === id2);
+console.log(id1 === id2);
+console.log(id1.toString(), id1.description);
 
-//console.log(id1.toString(), id1.description);
 
 let id = Symbol('id');
 let user = {
@@ -10,21 +10,19 @@ let user = {
 	[id]: 1
 };
 
-//console.log(user);
+console.log(user);
 
 for (let key in user) {
-	//console.log(user[key]);
+	console.log(user[key]);
 }
 
-let clone = Object.assign({}, user);
 
-//console.log(clone);
+let clone = Object.assign({}, user);
+console.log(clone);
 
 
 let entity = Symbol.for('spyder');
 let sameEntity = Symbol.for('spyder');
 
-//console.log(entity === sameEntity);
-
-//console.log(Symbol.keyFor(entity));
-
+console.log(entity === sameEntity);
+console.log(Symbol.keyFor(entity));
